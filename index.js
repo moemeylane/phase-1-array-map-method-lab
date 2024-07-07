c2b93fba2866c4eae7a893606f884a528f17a1f5
moemeylane <wangarimaureen668@gmail.com>
2024-07-07 21:41:33 +0300

    complete lab

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
-
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map(title => {
+    let words = title.split(' ');
+    words = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+    return words.join(' ');
+  });
 }
+ 
